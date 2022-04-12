e0b2f4d0d9140e3a52b826a978064dd335c8ebbe
Mariana Siano <maryhsiano@hotmail.com>
2022-04-12 20:09:31 -0300

    Atualizando codigo

--- CSharp/Primeiro/Primeiro/Program.cs
+++ CSharp/Primeiro/Primeiro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Primeiro
 {
@@ -8,16 +9,25 @@ namespace Primeiro
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Olá, pessoa!!");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
 
             double exemplo = 10.35784;
 
+            Console.WriteLine("Impressão normal");
             Console.WriteLine(exemplo);
+            Console.WriteLine("\n");
 
-            Console.WriteLine("Com duas casas descimais");
+            Console.WriteLine("Com duas casas decimais");
             Console.WriteLine(exemplo.ToString("F2"));
+            Console.WriteLine("\n");
 
-            Console.WriteLine("Com quatro casas descimais");
+            Console.WriteLine("Com quatro casas decimais");
             Console.WriteLine(exemplo.ToString("F4"));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Com uma função");
+            Console.WriteLine(exemplo.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
