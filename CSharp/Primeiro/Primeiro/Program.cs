3da2cac2cacf4969dcf675294d45e4646e2bbca7
Mariana Siano <maryhsiano@hotmail.com>
2022-04-12 17:41:48 -0300

    Atualizando o codigo

---
+++ CSharp/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primeiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá, pessoa!!");
+
+            double exemplo = 10.35784;
+
+            Console.WriteLine(exemplo);
+
+            Console.WriteLine("Com duas casas descimais");
+            Console.WriteLine(exemplo.ToString("F2"));
+
+            Console.WriteLine("Com quatro casas descimais");
+            Console.WriteLine(exemplo.ToString("F4"));
+        }
+    }
+}
