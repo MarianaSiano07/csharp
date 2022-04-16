ccf85a65088efd1a1bb0517b54736ab32a5de991
Mariana Siano <maryhsiano@hotmail.com>
2022-04-16 10:45:34 -0300

    Adicionando mais alguns exericios feitos

---
+++ CSharp/Primeiros_Passos/Codigo_Exercicio/Exercicio_Quatro/Exercicio_Quatro/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Quatro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicios Parte 2!!");
+            Console.WriteLine("\n");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("\n");
+            /*
+            Console.WriteLine("Exercício URI 1011");
+            Console.WriteLine("\n");
+
+            double raio;
+            double pi;
+            pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine());
+
+            double volume;
+
+            volume = (4.0 / 3) * pi * (raio * raio * raio);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("\n");
+            
+            Console.WriteLine("Exercício URI 1012");
+            Console.WriteLine("\n");
+
+            double A, B, C;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            A = double.Parse(valores[0]);
+            B = double.Parse(valores[1]);
+            C = double.Parse(valores[2]);
+
+            double triangulo, circulo, trapezio, quadrado, retangulo;
+
+            triangulo = A * C / 2.0;
+            circulo = 3.14159 * C * C;
+            trapezio = (A + B) / 2.0 * C;
+            quadrado = B * B;
+            retangulo = A * B;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("\n");
+            */
+            Console.WriteLine("Exercício URI 1013");
+            Console.WriteLine("\n");
+
+
+        }
+    }
+}
