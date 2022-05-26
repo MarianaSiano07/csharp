892873fe8d7976bd41fb7b66a1740177217c91d4
Mariana Siano <maryhsiano@hotmail.com>
2022-05-26 19:33:53 -0300

    Adicionando os arquivos do curso 'C#: Primeiros Passos'

---
+++ CSharp_Logica-Programacao/Codigo/Entrada_Dados/Entrada_Dados/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Entrada_Dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parte 1");
+            Console.WriteLine("\n");
+
+            string frase = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A frase escolhida foi: " + frase);
+            Console.WriteLine("\n");
+
+            string x = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A palavra escolhida foi: " + x);
+            Console.WriteLine("\n");
+
+            string y = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A palavra escolhida foi: " + y);
+            Console.WriteLine("\n");
+
+            string z = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A palavra escolhida foi: " + z);
+            Console.WriteLine("\n");
+
+            /*string s = Console.ReadLine();
+            Console.WriteLine("\n"); */
+           string[] vet = Console.ReadLine().Split(' ');
+
+            for(int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine("Vetor " + i + ": " + vet[i]);
+                Console.WriteLine("\n");
+            }
+
+            string a, b, c;
+            a = vet[0];
+            b = vet[1];
+            c = vet[2];
+
+            Console.WriteLine("Separando as palavras em variáveis!!");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A palavra escolhida foi: " + a);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A palavra escolhida foi: " + b);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A palavra escolhida foi: " + c);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Parte 2");
+            Console.WriteLine("\n");
+
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("O número escolhido foi: " + n1);
+            Console.WriteLine("\n");
+
+            char ch = char.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("A letra escolhido foi: " + ch);
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ CSharp_Logica-Programacao/Codigo/Operacoes_Matematicas/Operacoes_Matematicas/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Operacoes_Matematicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3.0;
+            double y = 4.0;
+            double z = -5.0;
+            double A, B, C;
+
+            Console.WriteLine("Raiz Quadrada!");
+            Console.WriteLine("\n");
+
+            A = Math.Sqrt(x);
+            Console.WriteLine("Resultade da raiz quadrada de " + x + " é de: " + A);
+            Console.WriteLine("\n");
+
+            B = Math.Sqrt(y);
+            Console.WriteLine("Resultade da raiz quadrada de " + y + " é de: " + B);
+            Console.WriteLine("\n");
+
+            C = Math.Sqrt(25.0);
+            Console.WriteLine("Resultade da raiz quadrada de 25.0 é de: " + C);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Potência!");
+            Console.WriteLine("\n");
+
+            double D, E, F;
+
+            D = Math.Pow(x, y);
+            Console.WriteLine("Resultade da potência de " + x + " com " + y + " é de: " + A);
+            Console.WriteLine("\n");
+
+            E = Math.Pow(y, 2.0);
+            Console.WriteLine("Resultade da potência de " + y + " com 2.0 é de: " + B);
+            Console.WriteLine("\n");
+
+            F = Math.Pow(5.0, 2.0);
+            Console.WriteLine("Resultade da potência de 5.0 com 2.0 é de: " + C);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Valor Absoluto ou Módulo!");
+            Console.WriteLine("\n");
+
+            double G, H;
+            G = Math.Abs(x);
+            Console.WriteLine("Resultade da potência de " + x + " é de: " + G);
+            Console.WriteLine("\n");
+
+            H = Math.Abs(z);
+            Console.WriteLine("Resultade da potência de " + z + " é de: " + H);
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Fórmula de Baskara");
+            Console.WriteLine("\n");
+
+            double delta;
+            delta = Math.Pow(y, 2.0) - 4 * x * z;
+
+            Console.WriteLine("Resultado do delta é: " + delta);
+            Console.WriteLine("\n");
+
+            double x1, x2;
+            x1 = (-y + Math.Sqrt(delta)) / (2.0 * x);
+            x2 = (y + Math.Sqrt(delta)) / (2.0 * x);
+
+            Console.WriteLine("Resultado do x1 é: " + x1);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Resultado do x2 é: " + x2);
+            Console.WriteLine("\n");
+        }
+    }
+}
